fef7a06ff24a8ea614fa3ff955565f5729414278
Kentaro Hirano <kntr365@gmail.com>
2015-03-28 15:26:59 +0900

    Initial commit

---
+++ chrome.go
@@ -0,0 +1,52 @@
+package chrome
+
+import (
+  "os"
+  "encoding/binary"
+  "io"
+)
+
+var byteOrder binary.ByteOrder = binary.LittleEndian
+
+func Post(msg []byte) error {
+  return post(msg, os.Stdout)
+}
+
+func post(msg []byte, writer io.Writer) error {
+  // Post message length in native byte order
+  header := make([]byte, 4)
+  byteOrder.PutUint32(header, (uint32)(len(msg)))
+  if n, err := writer.Write(header); err != nil || n != len(header) {
+    return err
+  }
+
+  // Post message body
+  if n, err := writer.Write(msg); err != nil || n != len(msg) {
+    return err
+  }
+  return nil
+}
+
+func Recieve() ([]byte, error) {
+  return recieve(os.Stdin)
+}
+
+func recieve(reader io.Reader) ([]byte, error) {
+  // Read message length in native byte order
+  var length uint32
+  if err := binary.Read(reader, byteOrder, &length); err != nil {
+    return nil, err
+  }
+
+  // Return if no message
+  if length == 0 {
+    return nil, nil
+  }
+
+  // Read message body
+  recieved := make([]byte, length)
+  if n, err := reader.Read(recieved); err != nil || n != len(recieved) {
+    return nil, err
+  }
+  return recieved, nil
+}
